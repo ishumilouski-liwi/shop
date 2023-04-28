1e19ce6ac2b7892c7823f7a8354a97de86bcc619
Ihar Shumilouski <ihar.shumilouski@livewire.com>
2023-04-28 15:36:17 +0300

    update shop fe

--- shop-fe/src/constants/apiPaths.ts
+++ shop-fe/src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://fdgdik89qi.execute-api.us-east-1.amazonaws.com/dev",
-  order: "https://fdgdik89qi.execute-api.us-east-1.amazonaws.com/dev",
-  import: "https://fdgdik89qi.execute-api.us-east-1.amazonaws.com/dev",
-  bff: "https://fdgdik89qi.execute-api.us-east-1.amazonaws.com/dev",
-  cart: "https://fdgdik89qi.execute-api.us-east-1.amazonaws.com/dev",
+  product: "https://1i3dtebhjd.execute-api.us-east-1.amazonaws.com/dev",
+  order: "https://1i3dtebhjd.execute-api.us-east-1.amazonaws.com/dev",
+  import: "https://1i3dtebhjd.execute-api.us-east-1.amazonaws.com/dev",
+  bff: "https://1i3dtebhjd.execute-api.us-east-1.amazonaws.com/dev",
+  cart: "https://1i3dtebhjd.execute-api.us-east-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
